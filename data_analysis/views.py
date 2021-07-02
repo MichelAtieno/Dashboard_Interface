48d032c1219ddff3b6242cbfdba943d4994d44f1
Michel Atieno <mishqamish@gmail.com>
2021-07-02 09:37:27 +0300

    User Pages based on permissions, Admin has different views from staff

--- data_analysis/views.py
+++ data_analysis/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required()
 def index(request):
     return render(request, "index.html")
 
+@login_required()
 def staff(request):
     return render(request, "staff.html")
 
+@login_required()
 def product(request):
     return render(request, "product.html")
 
+@login_required()
 def profile(request):
     return render(request, "profile.html")
 
+@login_required()
 def order(request):
     return render(request, "order.html")
